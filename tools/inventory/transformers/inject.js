697e39b5d084523413d001d8040ecfd21e01486c
Rachel Waterman <rwaterman@adobe.com>
2025-09-01 19:49:20 +0100

    inject script mod.

--- tools/inventory/transformers/inject.js
+++ tools/inventory/transformers/inject.js
@@ -4,4 +4,8 @@
   document.querySelector('.yellow-bar')?.remove();
   document.querySelector('#onetrust-consent-sdk')?.remove();
   document.querySelector('.skip-link')?.remove();
+
+  document.querySelectorAll('.header-home, .article-header, .header-page').forEach(el => {
+    el.setAttribute('data-block-type', 'hero');
+  });
 })();
