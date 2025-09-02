33e594596647e6d9dbdb9be3ab0f9e740bdacc53
Rachel Waterman <rwaterman@adobe.com>
2025-09-02 18:12:12 +0100

    Articles.

--- blocks/articles/articles.js
+++ blocks/articles/articles.js
@@ -1,7 +1,22 @@
 export default async function decorate(block) {
   const items = block.querySelectorAll(':scope > div');
   items.forEach((item) => {
-    const type = item.querySelector(':scope > div:first-child');
-    item.classList.add('articles__item', type.textContent.trim());
+    const typeContainer = item.querySelector(':scope > div:first-child');
+    const type = typeContainer.textContent.trim();
+    item.classList.add('articles__item', type);
+
+    const postType = item.querySelector(':scope > div:nth-child(5)');
+    postType.classList.add('articles__item__post-type');
+
+    if (type === 'tease') {
+      const linkContainer = item.querySelector(':scope > div:nth-child(4)');
+      const link = linkContainer.querySelector('a');
+      link.textContent = '';
+      link.parentElement.classList.remove('button-container');
+      link.classList.remove('button');
+      link.append(...([...item.children].filter((el) => el !== linkContainer)));
+    }
+
+    typeContainer.remove();
   });
 }
