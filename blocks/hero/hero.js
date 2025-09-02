a4ddfb392ce9c8417fb1070b499deb3591eb6205
Rachel Waterman <rwaterman@adobe.com>
2025-09-02 21:56:28 +0100

    Hero secondary image position.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,6 +2,10 @@ export default function decorate(block) {
   const heading = block.querySelector(':scope > div:nth-child(1)');
   const label = block.querySelector(':scope > div:nth-child(2)');
   const para = block.querySelector(':scope > div:nth-child(3)');
+  const imgContainer = block.querySelector(':scope > div:nth-child(4)');
+  imgContainer.classList.add('hero__image');
+  const secondaryImgContainer = block.querySelector(':scope > div:nth-child(5)');
+  secondaryImgContainer.classList.add('hero__secondary-image');
   const textContainer = document.createElement('div');
   textContainer.append(label, heading, para);
   textContainer.classList.add('hero__text-container');
