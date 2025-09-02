af04f821bd2e1fdfc7d6a3ca575dae74565719c4
Rachel Waterman <rwaterman@adobe.com>
2025-09-02 12:01:30 +0100

    Home hero.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,5 +1,18 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
 export default function decorate(block) {
-  if (!block.querySelector(':scope > div:first-child picture')) {
-    block.classList.add('no-image');
+  const imgSourceLink = block.querySelector(':scope > div:nth-child(4) a');
+  if (imgSourceLink) {
+    const img = createOptimizedPicture(imgSourceLink.href, '', true);
+    imgSourceLink.replaceWith(img);
   }
+
+  const heading = block.querySelector(':scope > div:nth-child(1)');
+  const label = block.querySelector(':scope > div:nth-child(2)');
+  const para = block.querySelector(':scope > div:nth-child(3)');
+  const textContainer = document.createElement('div');
+  textContainer.append(label, heading, para);
+  textContainer.classList.add('hero__text-container');
+
+  block.prepend(textContainer);
 }
