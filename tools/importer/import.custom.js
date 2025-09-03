f4cdadacb9714b2a705c6a8944b81fc6d803cc02
Sweta Barman <swbarman@adobe.com>
2025-09-03 11:51:23 -0400

    fix: remove cards block

--- tools/importer/import.custom.js
+++ tools/importer/import.custom.js
@@ -58,7 +58,9 @@ export const customTransformers = {
         document.querySelector('.return-block')?.remove();
         document.querySelector('.sidebar-article')?.remove();
         document.querySelector('.breadcrumbs-wrapper')?.remove();
-        document.querySelectorAll('.block-posts')?.remove();
+        document.querySelectorAll('.block-posts')?.forEach(element => {
+          element.remove();
+        });
       } catch (e) {
         console.warn('Failed to remove onetrust-consent-sdk', e);
       }
