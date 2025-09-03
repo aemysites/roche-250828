cdaca8853f9213281454aef2cc88e2868cfaaaa4
Sweta Barman <swbarman@adobe.com>
2025-09-03 11:46:03 -0400

    fix: remove breadcrumbs and block posts

--- tools/importer/import.custom.js
+++ tools/importer/import.custom.js
@@ -57,6 +57,8 @@ export const customTransformers = {
         document.querySelector('.comment-box')?.remove();
         document.querySelector('.return-block')?.remove();
         document.querySelector('.sidebar-article')?.remove();
+        document.querySelector('.breadcrumbs-wrapper')?.remove();
+        document.querySelector('.block-posts')?.remove();
       } catch (e) {
         console.warn('Failed to remove onetrust-consent-sdk', e);
       }
