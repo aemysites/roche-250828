eafd6ddcb39e0c9127635fcc3264eab3edcb8419
Rachel Waterman <rwaterman@adobe.com>
2025-09-08 09:15:32 +0100

    Remove comments.

--- scripts/utils.js
+++ scripts/utils.js
@@ -3,7 +3,6 @@
  * @param {string} [prefix] Location of placeholders
  * @returns {object} Window placeholders object
  */
-// eslint-disable-next-line import/prefer-default-export
 export async function fetchPageIndex(prefix = 'default') {
   window.pageIndex = window.pageIndex || {};
   if (!window.pageIndex[prefix]) {
@@ -20,11 +19,39 @@ export async function fetchPageIndex(prefix = 'default') {
           resolve(window.pageIndex[prefix]);
         })
         .catch(() => {
-          // error loading placeholders
           window.pageIndex[prefix] = {};
           resolve(window.pageIndex[prefix]);
         });
     });
   }
   return window.pageIndex[`${prefix}`];
+}
+
+/**
+ * Fetches the categories object.
+ * @param {string} [prefix] Location of placeholders
+ * @returns {object} Window placeholders object
+ */
+export async function fetchCategories(prefix = 'default') {
+  window.categories = window.categories || {};
+  if (!window.categories[prefix]) {
+    window.categories[prefix] = new Promise((resolve) => {
+      fetch(`${prefix === 'default' ? '' : prefix}/categories.json`)
+        .then((resp) => {
+          if (resp.ok) {
+            return resp.json();
+          }
+          return {};
+        })
+        .then((json) => {
+          window.categories[prefix] = json.data;
+          resolve(window.categories[prefix]);
+        })
+        .catch(() => {
+          window.categories[prefix] = {};
+          resolve(window.categories[prefix]);
+        });
+    });
+  }
+  return window.categories[`${prefix}`];
 }
